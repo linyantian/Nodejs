7847d35168ce5050bc361e7cd037db3153829012
linyantian <15011126117@163.com>
2017-11-15 16:00:28 +0800

    update

--- day03/fs/3_readdir.js
+++ day03/fs/3_readdir.js
@@ -1,9 +1,9 @@
 /**
  * Created by web-01 on 2017/11/14.
  */
-const fs = require('fs');
-
-fs.readdir('c:/xampp/htdocs/Framework_codes/Nodejs/day03/fs',(err,files)=>{ //读取目录结构
-    if(err) throw err;
-    console.log(files);
-});
+//const fs = require('fs');
+//
+//fs.readdir('c:/xampp/htdocs/Framework_codes/Nodejs/day03/fs',(err,files)=>{ //读取目录结构
+//    if(err) throw err;
+//    console.log(files);
+//});
