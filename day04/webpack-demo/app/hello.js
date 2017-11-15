a12caf06cf31d2a6299329c0c12ec2a7c12e68e3
linyantian <15011126117@163.com>
2017-11-15 17:04:02 +0800

    update

---
+++ day04/webpack-demo/app/hello.js
@@ -0,0 +1,8 @@
+/**
+ * Created by web-01 on 2017/11/15.
+ */
+module.exports = function () {
+    var hello= document.createElement('div');
+    hello.textContent = 'Hello, Webpack!';
+    return hello;
+};
