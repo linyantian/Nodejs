bedb3259021b0e8b9c6769de2ad51e3609c4df67
linyantian <15011126117@163.com>
2017-11-16 12:05:36 +0800

    update

--- day04/webpack-demo/webpack.config.js
+++ day04/webpack-demo/webpack.config.js
@@ -11,5 +11,37 @@ module.exports = {
         contentBase: "./public",
         historyApiFallback: true,
         inline: true //实时刷新
-    }
+    },
+		module: {
+			rules: [
+				{
+					test: /(\.jsx|\.js)$/,
+					use: {
+						loader: "babel-loader",
+						options: {
+							presets: [
+								"es2015", 
+								"react"
+							]
+						}
+					},
+					exclude: /node_modules/
+				},
+				{
+					test: /(\.css)$/,
+					use: [
+						{
+							loader: "style-loader"
+						},
+						{
+							loader: "css-loader"
+						}
+					],
+					exclude: /node_modules/
+				}
+			]
+		}
+		
+
+
 }
